1bd3413db70b0766033ba0effbf714619a00dfea
WillHoll <willhollingsworth15@yahoo.com>
2019-10-04 16:08:31 -0600

    destructing complete

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+const {color, make, model, year} = carDetails
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  const {title, firstName, lastName} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -43,7 +43,7 @@ function greeting( obj ) {
 
 
 
-////////// PROBLEM 3 //////////
+////////// PROBLEM 3 /////////
 
 /*
   Write a function called totalPopulation that will take in an object.
@@ -53,7 +53,10 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation(obj) {
+  const {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 
 
@@ -67,7 +70,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients(obj) {
+  const {carb, fat, protein} = obj;
+  const arr = [];
+  arr.push(carb, fat, protein);
+  return arr;
+}
 
 
 
@@ -85,7 +93,11 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers({first, second, third}) {
+  if(first < second && first < third) return first;
+  if(second < first && second < third) return second;
+  if(third < first && thrid < second) return third;
+}
 
 
 
@@ -97,6 +109,10 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+function numberGroups({a,b,c}) {
+  if(a.length > b.length && a.length > c.length) return a;
+  if(b.length > a.length && b.length > c.length) return b;
+  if(c.length > b.length && c.length > b.length) return c;
+}
 
 
